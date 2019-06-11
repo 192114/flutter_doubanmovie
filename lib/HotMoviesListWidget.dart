76e349bfa78780583c6408a08a422cbb567b6425
192114 <shadowSun192114@gmail.com>
2019-06-11 09:26:19 +0800

    添加网络请求

---
+++ lib/HotMoviesListWidget.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_douban/HotMovieData.dart';
+import 'package:flutter_douban/HotMovieItemWidget.dart';
+import 'package:http/http.dart' as http;
+
+class HotMoviesListWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HotMoviesListWidgetState();
+  }
+}
+
+// AutomaticKeepAliveClientMixin 不回收内存 tab切换时 不重新请求数据
+
+class HotMoviesListWidgetState extends State<HotMoviesListWidget> with AutomaticKeepAliveClientMixin {
+  List<HotMovieData> hotMovies = new List<HotMovieData>();
+
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  void _getData() async {
+    List<HotMovieData> serverDataList = new List();
+    var response = await http.get('https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=%E6%B7%B1%E5%9C%B3&start=0&count=10');
+    //成功获取数据
+    if (response.statusCode == 200) {
+      print(response.body);
+      var responseJson = json.decode(response.body);
+      for (dynamic data in responseJson['subjects']) {
+        HotMovieData hotMovieData = HotMovieData.fromJson(data);
+        serverDataList.add(hotMovieData);
+      }
+      setState(() {
+        if (mounted) {
+          hotMovies = serverDataList;
+        }
+      });
+    } 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (hotMovies == null || hotMovies.isEmpty) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return MediaQuery.removePadding(
+      removeTop: true,
+      context: context,
+      child: ListView.separated(
+        itemCount: hotMovies.length,
+        itemBuilder: (context, index) {
+          return HotMovieItemWidget(hotMovies[index]);
+        },
+        separatorBuilder: (context, index) {
+          return Divider(
+            height: 1,
+            color: Colors.black26,
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
